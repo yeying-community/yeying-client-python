bea015dfe49e440990c5d4c1b72cbce433cae536
youxuehu <youxuehu@hotmail.com>
2025-07-06 23:25:13 +0800

    fix

--- script/warehouse/pytest.sh
+++ script/warehouse/pytest.sh
@@ -2,6 +2,6 @@
 set -x
 # 指定服务端的 WAREHOUSE_ENDPOINT
 # 资产仓库服务端 API 地址
-export WAREHOUSE_ENDPOINT="http://localhost:8641"
+export WAREHOUSE_ENDPOINT="http://47.116.193.171:8641"
 pytest -s --log-cli-level=INFO --show-capture=all -v --durations=0
 set +x
