daeaaa13aab52fe7318387a6fd619db93a4d70f7
youxuehu <youxuehu@hotmail.com>
2025-07-08 23:11:23 +0800

    fix docs

--- source/conf.py
+++ source/conf.py
@@ -8,13 +8,19 @@
 
 project = 'yeying-client-python'
 copyright = '2025, yeying'
-author = 'yeying-community'
-release = 'yeying-client'
+author = 'yeying'
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../yeying'))  # 指向代码目录
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon"]
+extensions = [
+    'sphinx.ext.autodoc',    # 自动提取注释
+    'sphinx.ext.napoleon'    # 支持Google/Numpy风格注释
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,9 +30,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"  # 使用 ReadTheDocs 主题
+html_theme = 'sphinx_rtd_theme'  # 使用ReadTheDocs主题
 html_static_path = ['_static']
-
-import os
-import sys
-sys.path.insert(0, os.path.abspath("../../yeying"))  # 导入 SDK 代码
