7b7426b2357b2d4c8927a67179c326fe2152aa1f
youxuehu <youxuehu@hotmail.com>
2025-07-18 21:33:34 +0800

    fix docs deploy

--- script/warehouse/downloader.sh
+++ script/warehouse/downloader.sh
@@ -3,9 +3,14 @@ rm -rf build
 rm -rf dist
 rm -rf yeying_client.egg-info
 
+current_path=$(pwd)
+file_path=$current_path/yeying/version.py
+# 获取版本号
+version=$(awk -F"['\"]" '/__version__/{print $2; exit}' "$file_path")
+
 python setup.py bdist_wheel
 
-pip uninstall -y yeying_client && pip install dist/yeying_client-0.0.2-py3-none-any.whl
+pip uninstall -y yeying_client && pip install "dist/yeying_client-$version-py3-none-any.whl"
 
 # 指定服务端 WAREHOUSE_ENDPOINT
 # 资产仓库服务端 API 地址
